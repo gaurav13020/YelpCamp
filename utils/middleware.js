4ff6e6c78c279d05c5d9297d09effaa17c102527
Lu Li <greenpearll0804@gmail.com>
2026-03-18 11:00:00 -0700

    Add app middleware

---
+++ utils/middleware.js
@@ -0,0 +1,67 @@
+const {campgroundSchema, reviewSchema} = require('../schema')
+const ExpressError = require('./ExpressError');
+const Campground = require('../models/campground');
+const Review = require('../models/reviews');
+
+module.exports.isLoggedIn = (req,res,next)=>{
+    if(!req.isAuthenticated()){
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'You must be signed in first!');
+        res.redirect('/login');
+    }else{
+        next();
+    }
+    
+}
+
+module.exports.storeReturnTo = (req,res,next)=>{
+    if(req.session.returnTo){
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+//validating campground data
+module.exports.validateCampground = (req,res,next)=>{
+    const {error} = campgroundSchema.validate(req.body, { abortEarly: false });
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        return next(new ExpressError(msg,400));
+    }else{
+        next();
+    }
+}
+
+//checking if currentUser is authorized to make edit and delete
+module.exports.isAuthor = async (req,res,next)=>{
+    const camp = await Campground.findById(req.params.id);
+    if(!req.user._id.equals(camp.author)){
+        req.flash('error','You do not have the permission to perform the operation!');
+        return res.redirect(`/campground/${camp._id}`);
+    }
+    next();
+}
+
+//checking if currentUser is authorized to make edit and delete
+module.exports.isReviewAuthor = async (req,res,next)=>{
+    const {reviewId} = req.params;
+    const review = await Review.findById(reviewId);
+    if(!req.user._id.equals(review.author)){
+        req.flash('error','You do not have the permission to perform the operation!');
+        return res.redirect(`/campground/${req.params.id}`);
+    }
+    next();
+}
+
+
+//validating review data
+module.exports.validateReview= (req,res,next)=>{
+    const {error} = reviewSchema.validate(req.body, { abortEarly: false });
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        return next(new ExpressError(msg,400));
+    }else{
+        next();
+    }
+}
+
